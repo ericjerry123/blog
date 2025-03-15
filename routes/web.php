64b5343fa928c060039166c37317cc2c36baeb0a
Eric Chang <a1756972831@gmail.com>
2025-03-15 10:27:59 +0800

    feat: 新增文章資源路由，整合 PostController 以管理文章功能

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -25,6 +26,4 @@ Route::post('/register', [RegisterController::class, 'store'])->name('register.s
 Route::get('/login', [LoginController::class, 'create'])->name('login.create');
 Route::post('/login', [LoginController::class, 'store'])->name('login.store');
 
-Route::get('/posts', function () {
-    return view('posts.index');
-})->name('posts.index')->middleware('auth');
+Route::resource('posts', PostController::class);

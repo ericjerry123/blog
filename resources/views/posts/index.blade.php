d1c34e20c5c373cf07abac77ab8827313c7cc50e
Eric Chang <a1756972831@gmail.com>
2025-03-15 11:27:34 +0800

    feat: 新增 Alert 組件及相關視圖，整合 Session 提示功能以提升用戶體驗

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,18 +1,8 @@
 <x-layouts.app>
     <div class="container mx-auto px-4 py-8">
+        <x-session-alerts />
+
         <x-posts.header :title="'部落格文章'" />
-
-        @session('success')
-            <div class="alert alert-success mb-4">
-                {{ session('success') }}
-            </div>
-        @endsession
-
-        @session('error')
-            <div class="alert alert-error mb-4">
-                {{ session('error') }}
-            </div>
-        @endsession
 
         <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             @foreach ($posts as $post)

8f5b89e1e63c2cd724e2bd04d04cf70881411bdd
Eric Chang <a1756972831@gmail.com>
2025-03-15 14:16:09 +0800

    feat: 實現登出功能

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -40,34 +40,12 @@ class LoginController extends Controller
     }
 
     /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy()
     {
-        //
+        $this->loginService->logout();
+
+        return redirect()->route('login')->with('success', '登出成功');
     }
 }

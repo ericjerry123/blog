cb0c0d063934894dafb2a6adb5eab1daff4bf04b
Eric Chang <a1756972831@gmail.com>
2025-03-15 08:05:55 +0800

    feat: 新增 DaisyUI 套件及整合至 Tailwind CSS 配置

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,13 +6,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    @vite('resources/css/app.css')
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body>
     <h1 class="text-3xl font-bold underline">
         Hello world!
     </h1>
+
+    <button class="btn">Button</button>
 </body>
 
 </html>

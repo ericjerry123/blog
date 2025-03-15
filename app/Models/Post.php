8d739ef3fd4ee1437d3200902c6a1f8110366bdb
Eric Chang <a1756972831@gmail.com>
2025-03-15 13:16:29 +0800

    feat: 新增 Comment 模型及其與 Post 和 User 模型的關聯，並在 Post 模型中新增留言相關方法

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -20,4 +20,20 @@ class Post extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    /**
+     * 獲取文章的留言
+     */
+    public function comments()
+    {
+        return $this->hasMany(Comment::class)->whereNull('parent_id');
+    }
+
+    /**
+     * 獲取文章的所有留言（包括回覆）
+     */
+    public function allComments()
+    {
+        return $this->hasMany(Comment::class);
+    }
 }

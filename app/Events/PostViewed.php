83f1ba628168b6554c61e796cad956e3160c4cb2
Eric Chang <71639961+ericjerry123@users.noreply.github.com>
2025-03-17 20:43:06 +0800

    feat: 新增文章瀏覽次數功能 (#27)
    * feat: 新增文章查看次數欄位遷移至 posts 資料表
    
    * feat: 新增文章瀏覽事件及監聽器，實現文章瀏覽次數的自動增量計算
    
    * feat: 更新文章卡片及顯示頁面，新增文章瀏覽次數顯示功能

---
+++ app/Events/PostViewed.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Events;
+
+use App\Models\Post;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class PostViewed
+{
+    use Dispatchable, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     */
+    public function __construct(
+        public readonly Post $post
+    ) {}
+}
